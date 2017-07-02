baff824e9dddba44fc9bb07be0c9ccb1688fedb3
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-02 09:24:00 +0300

    create calendar wrapper and calendar table

--- src/datetime/calendar.js
+++ src/datetime/calendar.js
@@ -1,4 +1,6 @@
 import React, { PropTypes } from 'react';
+import { bindAll } from 'lodash';
+import classnames from 'classnames';
 
 export default class Calendar extends React.Component {
 
@@ -8,10 +10,50 @@ export default class Calendar extends React.Component {
         onChange: PropTypes.func.isRequired
     };
 
-    render() {
-        return (
-            <div>
+    constructor(props) {
+        super(props);
 
+        bindAll(this, ['previosMonth', 'nextMonth', 'renderCells']);
+
+        this.state = {
+            m: this.props.moment
+        }
+    }
+
+    previosMonth() {
+
+    }
+
+    nextMonth() {
+
+    }
+
+    renderCells() {
+
+    }
+
+    render() {
+        const { m } = this.state;
+        const wrapperClasses = classnames(this.props.className, 'tab-calendar');
+        const weeks = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+        return (
+            <div className={ wrapperClasses }>
+                <div className='toolbar'>
+                    <button className='prev-month' onClick={ this.previosMonth }> - </button>
+                    <span className='current-date'>{ m.format('MMM YYYY')}</span>
+                    <button className='next-month' onClick={ this.nextMonth }> - </button>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            { weeks.map((w, i) => <td key={i}>{ w }</td>) }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.renderCells }
+                    </tbody>
+                </table>
             </div>
         );
     }
