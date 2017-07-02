f8150e3aaa558b63d12408031d66ace038a4b606
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-02 08:56:02 +0300

    add Calendar PropTypes

--- src/datetime/calendar.js
+++ src/datetime/calendar.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export default class Calendar extends React.Component {
+
+    static propTypes = {
+        moment: PropTypes.any.isRequired,
+        className: PropTypes.string.isRequired,
+        onChange: PropTypes.func.isRequired
+    };
 
     render() {
         return (
